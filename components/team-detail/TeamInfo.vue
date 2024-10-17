09075736fc012e659fd735e8a82c1a9065b095d7
Md Haider Hossain <haider.dev.bd@gmail.com>
2024-10-17 18:07:53 +0600

    team-details page done

---
+++ components/team-detail/TeamInfo.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { social1 } from "~/data/data";
+import TeamForm from "./ui/TeamForm.vue";
+</script>
+
+<template>
+    <div class="section optech-section-padding">
+        <div class="container">
+            <div class="optech-team-single-wrap">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div
+                            class="optech-team-single-thumb"
+                            data-aos="fade-up"
+                            data-aos-duration="800"
+                        >
+                            <NuxtImg
+                                src="/assets/images/team/team-details.png"
+                                alt="team"
+                            />
+                        </div>
+                    </div>
+                    <div class="col-lg-6 offset-lg-1">
+                        <div class="optech-team-single-content">
+                            <h2>Sophia Rodriguez</h2>
+                            <span>CEO & Founder</span>
+                            <p>
+                                A content management system helps you create,
+                                manage, and publish content on the web. It also
+                                keep content organized and accessible so it can
+                                be used and repurposed effectively.
+                            </p>
+                            <div class="optech-footer-info dark-color">
+                                <ul>
+                                    <li>
+                                        <NuxtLink href="tel:123"
+                                            ><i class="ri-phone-fill"></i
+                                            >518-564-3200</NuxtLink
+                                        >
+                                    </li>
+                                    <li>
+                                        <NuxtLink href="mailto:name@email.com"
+                                            ><i class="ri-mail-fill"></i
+                                            >mthemeus@example.com</NuxtLink
+                                        >
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="optech-extra-mt">
+                                <div class="optech-social-icon-box style-two">
+                                    <ul>
+                                        <li
+                                            v-for="(item, i) in social1"
+                                            :key="i"
+                                        >
+                                            <NuxtLink :href="item.url">
+                                                <i :class="item.icon"></i>
+                                            </NuxtLink>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- team form start -->
+                        <TeamForm />
+                        <!-- team form end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/team-details/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Footer8 from "~/components/footer/Footer8.vue";
+import Header6 from "~/components/header/Header6.vue";
+import TeamInfo from "~/components/team-detail/TeamInfo.vue";
+import Breadcrumb from "~/components/ui/sections/Breadcrumb.vue";
+import WorkTogether2 from "~/components/ui/sections/WorkTogether2.vue";
+
+const path = [
+    {
+        label: "Home",
+        url: "/",
+    },
+    {
+        label: "Team",
+        url: "/team",
+    },
+    {
+        label: "Team Single",
+        url: "/",
+    },
+];
+
+useHead({
+    title: "Pricing",
+});
+</script>
+<template>
+    <Header6 />
+    <Breadcrumb title="Team Single" :path="path" />
+    <TeamInfo />
+    <WorkTogether2 />
+    <Footer8 />
+</template>
